42c1b6a696aa94d931146b91224497c344b93923
Brandon B <bmbagley@gmail.com>
2025-09-03 18:55:45 -0400

    Adding model select to the python file using lmstudio package

--- src/llmstudio_py_buildfun/config.py
+++ src/llmstudio_py_buildfun/config.py
@@ -1,13 +1,17 @@
 """Config for openai class build."""
 
+
 from typing import Literal
 
+import lmstudio as lms
 from pydantic import BaseModel
 
 
 # Define input to LLM class
 class LlmInput(BaseModel):
-    """Attributes:
+    """Class for requesting llm.
+
+    Attributes:
     model_name: LLM Model name to use
     role:  [user, system]
     content: query string
@@ -18,7 +22,9 @@ class LlmInput(BaseModel):
 
 
 def llm_request(client, content: LlmInput):
-    """Args:
+    """Generate response from llm request.
+
+    Args:
         client (): OpenAI client
         content: type and input of request
 
@@ -37,5 +43,47 @@ def llm_request(client, content: LlmInput):
         print(f"Error getting response\n{e}")
 
 def parse_llm_out(response):
+    """Func to parse the message from an llm response.
+
+    Args:
+        response (): output from openai model json
+
+    Returns: String of llm output
+
+    """
     # print(response.choices[0].message.content)
     return response.choices[0].message.content
+
+
+def return_downloaded_model(model_type='llm') -> list[lms.AnyDownloadedModel]:
+    """Generate list of models downloaded in lmstudio.
+
+    Args:
+        model_type (): llm or embedding
+
+    Returns: list of lmstudio models
+
+    """
+    dld = lms.list_downloaded_models(namespace=model_type)
+    return dld
+
+
+def user_input_from_list(option_list:list):
+    """Function that generates a choose option for users, using a supplied list.
+
+    Args:
+        option_list: list of options for a user to choose
+    """
+    user_input = ''
+    input_message = "Type out your option:\n"
+
+    for index, item in enumerate(option_list):
+        input_message += f'{index+1}) {item}\n'
+
+    input_message += 'Your choice: '
+
+    while user_input not in map(str, range(1, len(option_list)+1)):
+        user_input = input(input_message)
+    # while user_input.lower() not in options:
+    #     user_input = input(input_message)
+    print('You picked: ' + option_list[int(user_input) -1])
--- src/llmstudio_py_buildfun/openai_easyquery.py
+++ src/llmstudio_py_buildfun/openai_easyquery.py
@@ -1,7 +1,14 @@
 """Testing responses from a locally hosted LLM via LMStudio using the OpenAi package."""
 
-from config import LlmInput, llm_request, parse_llm_out
 from openai import OpenAI
+
+from llmstudio_py_buildfun.config import (
+    LlmInput,
+    llm_request,
+    parse_llm_out,
+    return_downloaded_model,
+    user_input_from_list,
+)
 
 client = OpenAI(
     base_url="http://localhost:1234/v1",
@@ -17,9 +24,12 @@ client = OpenAI(
 
 
 if __name__ == "__main__":
+    dld_models = return_downloaded_model()
+    # dld_model_names =
+    model_select = user_input_from_list(option_list=[''.join(i.model_key) for i in dld_models])
     user_input = input("Type Question: ")
-    request_in = LlmInput(model_name="liquid/lfm2-1.2b", role="user", \
-                          content=user_input)
+    request_in = LlmInput(model_name=str(model_select), role="user", \
+                          content=str(user_input))
 
     response = llm_request(client, request_in)
     print(parse_llm_out(response))
